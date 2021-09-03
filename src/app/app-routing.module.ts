8651a290e60a8d72fcdeba9b2c8041cc93f5e7c8
oscargon12 <oscargon12@gmail.com>
2021-09-03 12:38:14 -0500

    subiendo componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NewsComponent } from './components/news/news.component';
+import { SignComponent } from './components/sign/sign.component';
+import { Error404Component } from './components/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: '/home', pathMatch: "full" }, // Por si no hay nada después del /
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "news", component: NewsComponent },
+  { path: "sign", component: SignComponent },
+  { path: "error404", component: Error404Component}, // Error 404 component
+  { path: "**", redirectTo: '/error404', pathMatch: "full" } // por si hay un para que me dirija al error 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NewsComponent } from './components/news/news.component';
+import { SignComponent } from './components/sign/sign.component';
+import { Error404Component } from './components/error404/error404.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    NewsComponent,
+    SignComponent,
+    Error404Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
